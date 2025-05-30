09c0a875e0d0948298313631ea27c4995babb637
Shivani Balaji <shivanibalaji2612@gmail.com>
2025-05-30 13:23:47 +0300

    login and access token handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,28 @@
+'use client'
+
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useEffect } from "react";
+// import { URLSearchParams } from "next/dist/compiled/@edge-runtime/primitives/url";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(()=>{
+    var url = new URLSearchParams(window.location.search);
+    var at = url.has("access_token");
+    localStorage.setItem("access_token", url.get("access_token"))
+  }, [router.events]);
+
+
+  async function onClickofButton(){
+    try{
+      window.location.href = "http://localhost:5000/login";
+    }catch(error){
+      console.error(error.message)
+    }
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -26,11 +48,8 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
@@ -40,15 +59,13 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
+          </button>
+          <button
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={onClickofButton}
           >
             Read our docs
-          </a>
+          </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
